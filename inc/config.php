1b6f3914557bd62860f9fbd66c7abe8c29cfd753
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2024-01-02 21:52:22 +0300

    Adding the include files config and navbar

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+    $host = 'localhost';        // Server
+    $user = 'root';             // User
+    $password = '';             // Password
+    $db = 'pasi';               // Database name
+
+    $conn = mysqli_connect($host, $user, $password, $db);
+?>
---
+++ inc/navbar.php
@@ -0,0 +1,48 @@
+<!-- ======= Header ======= -->
+<nav class="navbar navbar-expand-lg bg-body-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img height="90" width="170" src="../assets/img/pasi_logo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active disabled" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle disabled" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        About Us
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">About PASI</a></li>
+                        <li><a class="dropdown-item" href="#">Our Core Values</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Services</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Trainings
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="training-calendar.php">Training Calendar</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Annual Conference</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Industry</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Header -->
